86052868352aeb77a3f5f4e1959ad50e3e809837
khaandev <mkhan.thecoder@gmail.com>
2025-12-22 08:53:52 +0300

    setup login/register ui

---
+++ app/pages/auth/login.vue
@@ -0,0 +1,56 @@
+<template>
+  <FormLayout>
+    <template #heading>Login</template>
+    <template #main>
+      <form class="space-y-4" @submit.prevent="handleLogin">
+        <div>
+          <BaseInput
+            v-model="form.email"
+            label="Email"
+            type="email"
+            placeholder="examlpe@gmail.com"
+            class="text-gray-500"
+          />
+        </div>
+
+        <div>
+          <BaseInput
+            v-model="form.password"
+            label="Password"
+            type="password"
+            placeholder="*******"
+            class="text-gray-500"
+          />
+        </div>
+
+        <div class="text-sm">
+          <a href="#" class="text-blue-500 hover:underline">Forgot password?</a>
+        </div>
+
+        <PrimaryButton type="submit"> Login </PrimaryButton>
+      </form>
+    </template>
+
+    <!--    <p class="mt-6 text-center text-sm text-gray-600">-->
+    <!--      Don’t have an account?-->
+    <!--      <Nuxt-link to="/guest/register" class="text-blue-500 hover:underline">Sign up</Nuxt-link>-->
+    <!--    </p>-->
+  </FormLayout>
+</template>
+
+<script setup>
+  // import FormLayout from "~/layouts/Guest/FormLayout.vue";
+  // import login from '~/pages/auth/login.vue'
+  // const auth = useAuthStore();
+  import FormLayout from '~/layouts/guest/FormLayout.vue'
+
+  const form = reactive({
+    email: 'khan@gmail.com',
+    password: 'password',
+  })
+
+  const handleLogin = async () => {
+    // await auth.attemptLogin(form);
+    console.log(form)
+  }
+</script>
---
+++ app/pages/auth/register.vue
@@ -0,0 +1,96 @@
+<template>
+  <FormLayout>
+    <template #heading>Register</template>
+    <template #main>
+      <form class="space-y-4" @submit.prevent="handleRegister">
+        <div>
+          <BaseInput
+            v-model="form.name"
+            label="Full Name"
+            type="text"
+            class="text-gray-500"
+            placeholder="John Doe"
+          />
+          <!--        <ValidationError field="name" />-->
+        </div>
+
+        <div>
+          <BaseInput
+            v-model="form.email"
+            label="Email"
+            class="text-gray-500"
+            type="email"
+            placeholder="example@gmail.com"
+          />
+          <!--        <ValidationError field="email" />-->
+        </div>
+        <div>
+          <BaseInput
+            v-model="form.password"
+            label="Password"
+            class="text-gray-500"
+            type="password"
+            placeholder="********"
+          />
+          <!--        <ValidationError field="password" />-->
+        </div>
+        <div>
+          <div>
+            <BaseInput
+              v-model="form.c_password"
+              class="text-gray-500"
+              label="Re-type Password"
+              type="password"
+              placeholder="********"
+            />
+          </div>
+        </div>
+
+        <PrimaryButton type="submit"> Register </PrimaryButton>
+      </form>
+    </template>
+    <!--    <p class="mt-6 text-center text-sm text-gray-600">-->
+    <!--      Already have an account?-->
+    <!--      <Nuxt-link to="/guest/login" class="text-blue-500 hover:underline">Log in</Nuxt-link>-->
+    <!--    </p>-->
+  </FormLayout>
+</template>
+
+<script setup>
+  import FormLayout from '~/layouts/guest/FormLayout.vue'
+
+  const form = reactive({
+    name: '',
+    email: '',
+    password: '',
+    c_password: '',
+  })
+  const handleRegister = async () => {
+    console.log(form)
+  }
+
+  //
+  // const handleRegister = async () => {
+  //   const data = {
+  //     name: form.name,
+  //     username: form.name,
+  //     email: form.email,
+  //     password: form.password,
+  //     password_confirmation: form.c_password,
+  //   };
+  //
+  //   loading.value = true;
+  //   const isSuccess = await auth.attemptRegister(data);
+  //   loading.value = false;
+  //   if (isSuccess) {
+  //     form.name = '';
+  //     form.email = '';
+  //     form.password = '';
+  //     form.c_password = '';
+  //   }
+  //
+  // };
+  // definePageMeta({
+  //   middleware: ["guest"],
+  // });
+</script>
